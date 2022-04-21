e8425b472e6f98ebcfae04ee40cf19807421797b
dhanajitkapali <dhanajitkapali30@gmail.com>
2022-04-22 03:05:09 +0530

    added the .env

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,10 +1,21 @@
 import { View, Text } from "react-native";
 import React, { createContext, useContext } from "react";
+import * as Google from "expo-google-app-auth";
+import { IOS_CLIENT_ID, ADNROID_CLIENT_ID } from "@env";
 
 const AuthContext = createContext();
+
+const config = {
+  iosClientId: IOS_CLIENT_ID,
+  scopes: ["profile", "email"],
+  permission: ["public_profile", "email", "gender", "location"],
+};
+
 export const AuthProvider = ({ children }) => {
+  const signInWithGoogle = async () => {};
+
   return (
-    <AuthContext.Provider value={{ user: "Dhanajit" }}>
+    <AuthContext.Provider value={{ user: null }}>
       {children}
     </AuthContext.Provider>
   );
