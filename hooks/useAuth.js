c921d5d8f816c575b72f88c82feda6fe1399cca1
dhanajitkapali <dhanajitkapali30@gmail.com>
2022-04-25 16:33:41 +0530

    added google signIn

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,11 +1,12 @@
 import { View, Text } from "react-native";
 import React, { createContext, useContext } from "react";
 import * as Google from "expo-google-app-auth";
-import { IOS_CLIENT_ID, ADNROID_CLIENT_ID } from "@env";
+import { IOS_CLIENT_ID, ANDROID_CLIENT_ID } from "@env";
 
 const AuthContext = createContext();
 
 const config = {
+  androidClientId: ANDROID_CLIENT_ID,
   iosClientId: IOS_CLIENT_ID,
   scopes: ["profile", "email"],
   permission: ["public_profile", "email", "gender", "location"],
