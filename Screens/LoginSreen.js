fc0d731c3a897e146cd1f3c72e4ef66eaef6f030
dhanajitkapali <dhanajitkapali30@gmail.com>
2022-04-25 18:23:56 +0530

    built the login screen

--- Screens/LoginSreen.js
+++ Screens/LoginSreen.js
@@ -1,13 +1,54 @@
-import { Button, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  ActivityIndicator,
+  Button,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useLayoutEffect } from "react";
 import useAuth from "../hooks/useAuth";
+import { useNavigation } from "@react-navigation/core";
 
 const LoginSreen = () => {
-  const { signInWithGoogle } = useAuth();
+  const { signInWithGoogle, loading } = useAuth();
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  });
+
   return (
-    <View>
-      <Text>LoginSreen</Text>
-      <Button title="Login" onPress={signInWithGoogle} />
+    <View style={{ flex: 1 }}>
+      <ImageBackground
+        resizeMode="cover"
+        style={{ flex: 1 }}
+        source={{ uri: "https://tinder.com/static/tinder.png" }}
+      >
+        <TouchableOpacity
+          onPress={signInWithGoogle}
+          style={{
+            backgroundColor: "white",
+            marginHorizontal: "25%",
+            marginVertical: "160%",
+            height: 45,
+            padding: 5,
+            borderRadius: 15,
+            alignContent: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Text
+            style={{ alignSelf: "center", fontSize: 20, fontWeight: "600" }}
+          >
+            Login
+          </Text>
+        </TouchableOpacity>
+        {/* {!loading && <ActivityIndicator size="large" />} */}
+      </ImageBackground>
     </View>
   );
 };
