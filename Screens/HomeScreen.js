5a9b5b4c09e7319c83ac771264f283f1039392ad
dhanajitkapali <dhanajitkapali30@gmail.com>
2022-04-25 20:25:02 +0530

    built HeaderView in HomeScreen

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,17 +1,59 @@
-import { Button, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/core";
 import useAuth from "../hooks/useAuth";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
-  const { logOut } = useAuth();
+  const { user, logOut } = useAuth();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  });
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-      <Button title="Chat" onPress={() => navigation.navigate("Chat")} />
-      <Button title="Logout" onPress={logOut} />
-    </View>
+    <SafeAreaView>
+      {/* HeaderView */}
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <TouchableOpacity style={{ marginLeft: 10 }} onPress={logOut}>
+          <Image
+            source={{ uri: user.photoURL }}
+            style={{ height: 40, width: 40, borderRadius: 20 }}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image
+            source={require("../src/images/tinder-logo-96.png")}
+            style={{ height: 60, width: 60 }}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{ marginRight: 10 }}
+          onPress={() => navigation.navigate("Chat")}
+        >
+          <Image
+            source={require("../src/images/tinder-chat-90.png")}
+            style={{ height: 40, width: 40 }}
+          />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
