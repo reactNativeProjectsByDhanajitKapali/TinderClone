37e6bc612962d5467c8c898e7f61d1987f074060
dhanajitkapali <dhanajitkapali30@gmail.com>
2022-04-26 16:28:27 +0530

    added the bottom buttons in HomeScreen

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -7,14 +7,16 @@ import {
   TouchableOpacity,
   Image,
 } from "react-native";
-import React, { useLayoutEffect } from "react";
+import React, { useLayoutEffect, useRef } from "react";
 import { useNavigation } from "@react-navigation/core";
 import useAuth from "../hooks/useAuth";
 import Swiper from "react-native-deck-swiper";
+import { Entypo } from "react-native-vector-icons";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
   const { user, logOut } = useAuth();
+  const swiperRef = useRef(null);
 
   const DUMMY_DATA = [
     {
@@ -107,14 +109,47 @@ const HomeScreen = () => {
       {/* HeaderView End */}
 
       {/* CardView Start*/}
-      <View style={{ flex: 1, backgroundColor: "black" }}>
+      <View
+        style={{
+          flex: 1,
+          height: "90%",
+          backgroundColor: "black",
+          flexDirection: "column-reverse",
+        }}
+      >
         <Swiper
+          ref={swiperRef}
           cards={DUMMY_DATA}
           containerStyle={{ backgroundColor: "white" }}
           stackSize={5}
           cardIndex={0}
           animateCardOpacity={true}
           verticalSwipe={false}
+          onSwipedLeft={() => {
+            console.log("Swiped NOPE");
+          }}
+          onSwipedRight={() => {
+            console.log("Swiped MATCH");
+          }}
+          overlayLabels={{
+            left: {
+              title: "NOPE",
+              style: {
+                label: {
+                  textAlign: "right",
+                  color: "red",
+                },
+              },
+            },
+            right: {
+              title: "MATCH",
+              style: {
+                label: {
+                  color: "#4DED30",
+                },
+              },
+            },
+          }}
           renderCard={(card) => (
             <View
               key={card}
@@ -151,13 +186,13 @@ const HomeScreen = () => {
                   <Text style={{ fontWeight: "600", fontSize: 20 }}>
                     {card.firstName} {card.lastName}
                   </Text>
-                  <Text style={{ fontSize: 16 }}>{card.occupation}</Text>
+                  <Text style={{ fontSize: 15 }}>{card.occupation}</Text>
                 </View>
                 <View
                   style={{
                     alignItems: "center",
                     justifyContent: "center",
-                    marginRight: 10,
+                    marginRight: 25,
                   }}
                 >
                   <Text
@@ -174,6 +209,43 @@ const HomeScreen = () => {
             </View>
           )}
         />
+
+        <View
+          style={{
+            flexDirection: "row",
+            //backgroundColor: "red",
+            height: "10%",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+          }}
+        >
+          <TouchableOpacity
+            onPress={() => swiperRef.current.swipeLeft()}
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+              height: 55,
+              width: 55,
+              backgroundColor: "#FF9999",
+              borderRadius: 27.5,
+            }}
+          >
+            <Entypo name="cross" size={30} color="red" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => swiperRef.current.swipeRight()}
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+              height: 55,
+              width: 55,
+              backgroundColor: "#B4E197",
+              borderRadius: 27.5,
+            }}
+          >
+            <Entypo name="heart" size={30} color="green" />
+          </TouchableOpacity>
+        </View>
       </View>
       {/* CardView End*/}
     </SafeAreaView>
